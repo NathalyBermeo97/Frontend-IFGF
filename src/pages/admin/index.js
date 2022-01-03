005885911e6ca5356aced21c3e129d0741f4e2df
NathalyBermeo97 <nathaly.bermeo@epn.edu.ec>
2022-01-03 13:24:45 -0500

    news delete

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,137 +1,14 @@
-import { withPrivate } from "../../hocs/withPrivate";
-import { useState } from "react";
-import { useNews } from "../../hooks/useNews";
-import { ListGroup, Button } from "react-bootstrap";
-import { UpdateNewsItemModal } from "../../components/UpdateNewsItemModal";
-import styles from "./styles.module.css";
-import { CreateNewsItemModal } from "../../components/CreateNewsItemModal";
-import { ERROR_MESSAGES, SERVER_RESPONSE } from "../../constans/inidex";
-import { ListOfNews } from "../../components/ListOfNews";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
-import * as yup from "yup";
-
-const newsItemSchema = yup.object().shape({
-  title: yup
-    .string()
-    .required(ERROR_MESSAGES.REQUIRED("título"))
-    .matches(/^[A-Za-z0-9!@#$%_\-^&*]+/, ERROR_MESSAGES.MATCH),
-  description: yup
-    .string()
-    .required(ERROR_MESSAGES.REQUIRED("descripción"))
-    .matches(/^[A-Za-z0-9!@#$%_\-^&*]+/, ERROR_MESSAGES.MATCH),
-});
+import React from 'react'
+import { Layout } from '../../components/Layout'
+import { withPrivate } from '../../hocs/withPrivate'
 
 const Admin = () => {
-  const { news, setNews, updateNews, createNewsItem } = useNews();
-  const [showModal, setShowModal] = useState(false);
-  const [showCreateNewsItemModal, setShowCreateNewsItemModal] = useState(false);
+    return (
+        <div>
+            <Layout />
+        </div>
+    )
+}
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-    clearErrors,
-  } = useForm({
-    defaultValues: {
-      title: "",
-      description: "",
-    },
-    resolver: yupResolver(newsItemSchema),
-  });
+export default Admin
 
-  const updateNewsItemForm = useForm({
-    resolver: yupResolver(newsItemSchema),
-  });
-
-  const onShowModal = (newsItem) => {
-    updateNewsItemForm.reset(newsItem);
-    setShowModal(true);
-  };
-
-  console.log({ errors });
-  const onSubmit = (data) => {
-    console.log(data);
-    createNewsItem(data).then((message) => {
-      if (message === SERVER_RESPONSE.NEWS_ITEM_CREATED) {
-        //UPDATE THIS WITH THE NEW RESPONSE RATHER THAN THE MESSAGE
-        setNews((prevState) => [
-          ...prevState,
-          { _id: Math.floor(Math.random() * 1000000000000), ...data },
-        ]);
-        setShowCreateNewsItemModal(false);
-        reset();
-      }
-    });
-    // setNews((prevState) => [
-    //   ...prevState,
-    //   { _id: Math.floor(Math.random() * 1000000000000), ...data },
-    // ]);
-    // setShowCreateNewsItemModal(false);
-    // reset();
-  };
-
-  const onSubmitUpdateNewsItem = (data) => {
-    const { _id: id } = data;
-    updateNews(id, data).then((message) => {
-      if (message === "Noticia actualizada correctamente") {
-        const newNews = news.map((newsItem) =>
-          newsItem._id === data._id ? data : newsItem
-        );
-        setNews(newNews);
-        setShowModal(false);
-      }
-    });
-    // const newNews = news.map((newsItem) =>
-    //   newsItem._id === data._id ? data : newsItem
-    // );
-    // setNews(newNews);
-    // setShowModal(false);
-  };
-
-  return (
-    <>
-      <div className={styles.newsHeader}>
-        <h2>Noticias</h2>
-        <Button
-          variant="outline-primary"
-          size="sm"
-          onClick={() => setShowCreateNewsItemModal(true)}
-        >
-          Crear
-        </Button>
-      </div>
-      <ListGroup as="ol" numbered>
-        {news.map((newsItem) => (
-          <ListOfNews
-            key={newsItem._id}
-            newsItem={newsItem}
-            onShowModal={onShowModal}
-          />
-        ))}
-      </ListGroup>
-
-      <UpdateNewsItemModal
-        show={showModal}
-        setShowModal={setShowModal}
-        register={updateNewsItemForm.register}
-        handleSubmit={updateNewsItemForm.handleSubmit}
-        onSubmit={onSubmitUpdateNewsItem}
-        errors={updateNewsItemForm.formState.errors}
-      />
-
-      <CreateNewsItemModal
-        showModal={showCreateNewsItemModal}
-        setShowModal={setShowCreateNewsItemModal}
-        register={register}
-        handleSubmit={handleSubmit}
-        onSubmit={onSubmit}
-        errors={errors}
-        clearErrors={clearErrors}
-      />
-    </>
-  );
-};
-
-export default withPrivate(Admin);
---
+++ src/pages/admin/news/index.js
@@ -0,0 +1,164 @@
+import { withPrivate } from "../../../hocs/withPrivate";
+import { useEffect, useState } from "react";
+import { useNews } from "../../../hooks/useNews";
+import { ListGroup, Button, FormControl, InputGroup } from "react-bootstrap";
+import { UpdateNewsItemModal } from "../../../components/UpdateNewsItemModal";
+import styles from "./styles.module.css";
+import { CreateNewsItemModal } from "../../../components/CreateNewsItemModal";
+import { ERROR_MESSAGES, SERVER_RESPONSE } from "../../../constans/inidex";
+import { ListOfNews } from "../../../components/ListOfNews";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
+import * as yup from "yup";
+
+const newsItemSchema = yup.object().shape({
+  title: yup
+    .string()
+    .required(ERROR_MESSAGES.REQUIRED("título"))
+    .matches(/^[A-Za-z0-9!@#$%_\-^&*]+/, ERROR_MESSAGES.MATCH),
+  description: yup
+    .string()
+    .required(ERROR_MESSAGES.REQUIRED("descripción"))
+    .matches(/^[A-Za-z0-9!@#$%_\-^&*]+/, ERROR_MESSAGES.MATCH),
+});
+
+const NewsPage = () => {
+  const { news, setNews, updateNews, createNewsItem } = useNews();
+  const [showModal, setShowModal] = useState(false);
+  const [showCreateNewsItemModal, setShowCreateNewsItemModal] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  const [filteredNews, setFilteredNews] = useState([]);
+
+  useEffect(() => {
+    const filteredNews = news.filter((ni) =>
+      ni.title.toLowerCase().includes(keyword.toLowerCase())
+    );
+    setFilteredNews(filteredNews)
+  }, [keyword, news]);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    clearErrors,
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+    resolver: yupResolver(newsItemSchema),
+  });
+
+  const updateNewsItemForm = useForm({
+    resolver: yupResolver(newsItemSchema),
+  });
+
+  const onShowModal = (newsItem) => {
+    updateNewsItemForm.reset(newsItem);
+    setShowModal(true);
+  };
+
+  const handleDelete = (id) => {
+    const newNews = news.filter(ni => ni._id !== id)
+    setNews(newNews)
+  }
+
+  console.log({ errors });
+  const onSubmit = (data) => {
+    console.log(data);
+    // createNewsItem(data).then((message) => {
+    //   if (message === SERVER_RESPONSE.NEWS_ITEM_CREATED) {
+    //     //UPDATE THIS WITH THE NEW RESPONSE RATHER THAN THE MESSAGE
+    //     setNews((prevState) => [
+    //       ...prevState,
+    //       { _id: Math.floor(Math.random() * 1000000000000), ...data },
+    //     ]);
+    //     setShowCreateNewsItemModal(false);
+    //     reset();
+    //   }
+    // });
+    setNews((prevState) => [
+      ...prevState,
+      { _id: Math.floor(Math.random() * 1000000000000), ...data },
+    ]);
+    setShowCreateNewsItemModal(false);
+    reset();
+  };
+
+  const onSubmitUpdateNewsItem = (data) => {
+    const { _id: id } = data;
+    // updateNews(id, data).then((message) => {
+    //   if (message === "Noticia actualizada correctamente") {
+    //     const newNews = news.map((newsItem) =>
+    //       newsItem._id === data._id ? data : newsItem
+    //     );
+    //     setNews(newNews);
+    //     setShowModal(false);
+    //   }
+    // });
+    const newNews = news.map((newsItem) =>
+      newsItem._id === data._id ? data : newsItem
+    );
+    setNews(newNews);
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <div className={styles.newsHeader}>
+        <h2>Noticias</h2>
+        <Button
+          variant="outline-primary"
+          size="sm"
+          onClick={() => setShowCreateNewsItemModal(true)}
+        >
+          Crear
+        </Button>
+      </div>
+
+      <InputGroup className="mb-3" style={{ padding: "15px" }}>
+        <FormControl
+          placeholder="Buscar Noticia"
+          aria-label="search_new"
+          aria-describedby="basic-addon1"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </InputGroup>
+
+      <ListGroup as="ol" numbered>
+        {filteredNews
+          .map((newsItem) => (
+            <ListOfNews
+              key={newsItem._id}
+              newsItem={newsItem}
+              onShowModal={onShowModal}
+              handleDelete={handleDelete}
+            />
+          ))}
+      </ListGroup>
+
+      <UpdateNewsItemModal
+        show={showModal}
+        setShowModal={setShowModal}
+        register={updateNewsItemForm.register}
+        handleSubmit={updateNewsItemForm.handleSubmit}
+        onSubmit={onSubmitUpdateNewsItem}
+        errors={updateNewsItemForm.formState.errors}
+      />
+
+      <CreateNewsItemModal
+        showModal={showCreateNewsItemModal}
+        setShowModal={setShowCreateNewsItemModal}
+        register={register}
+        handleSubmit={handleSubmit}
+        onSubmit={onSubmit}
+        errors={errors}
+        clearErrors={clearErrors}
+      />
+    </>
+  );
+};
+
+export default NewsPage;
