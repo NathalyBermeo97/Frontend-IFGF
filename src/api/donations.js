25d066173a0234d9729b181098c2969aa2b2868b
Nathaly Bermeo <nathaly.bermeo@epn.edu.ec>
2022-01-07 10:56:07 -0500

    routes to plural

--- src/api/donations.js
+++ src/api/donations.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 const Donations = {
     create: (newDonationsItem) => {
-        return api.post('/donation', newDonationsItem)
+        return api.post('/donations', newDonationsItem)
     }
 };
 
