9faf38f1cdc88a87e4bd940d566922d5eb37499a
NathalyBermeo97 <nathaly.bermeo@epn.edu.ec>
2022-01-19 17:36:12 -0500

    create questionnaires

--- src/api/questionnaires.js
+++ src/api/questionnaires.js
@@ -4,6 +4,9 @@ const Questionnaires = {
     get: () => {
         return api.get("/questionaries");
     },
+    create: (newQuestionnaire) => {
+        return api.post('/questionaries', newQuestionnaire)
+    }
 };
 
 export default Questionnaires;
