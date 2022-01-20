c2479d8db9b27f86d79bc1b62cd185c2efe14c80
NathalyBermeo97 <nathaly.bermeo@epn.edu.ec>
2022-01-20 16:35:35 -0500

    games attempt

--- src/api/questionnaires.js
+++ src/api/questionnaires.js
@@ -1,12 +1,12 @@
 import api from "./api";
 
 const Questionnaires = {
-    get: () => {
-        return api.get("/questionaries");
-    },
-    create: (newQuestionnaire) => {
-        return api.post('/questionaries', newQuestionnaire)
-    }
+  get: () => {
+    return api.get("/questionaries");
+  },
+  create: (newQuestionnaire) => {
+    return api.post("/questionaries", newQuestionnaire);
+  },
 };
 
 export default Questionnaires;
--- src/pages/game/[id].js
+++ src/pages/game/[id].js
@@ -1,10 +1,87 @@
-import { useRouter } from "next/router";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import api from "../../api/api";
+import Game from "../../api/score";
+import { ListOfQuestions } from "../../components/ListOfQuestions";
+import { Result } from "../../components/Result";
+import styles from "./style.module.css";
 
-const Game = () => {
-  const router = useRouter();
-  const {query: {id}} = router;
+const GamePage = ({ questionsBank }) => {
+  const [questionnaire, setQuestionnaire] = useState(questionsBank);
+  const [score, setScore] = useState(0);
+  const [responses, setResponses] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [isPlayingAgain, setIsPlayingAgain] = useState(false)
 
-  return <div>Game with id: {id}</div>;
+  const computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      setScore((prevState) => prevState + 1);
+    }
+    setResponses((prevState) => (prevState < 5 ? prevState + 1 : 5));
+  };
+
+  const playAgain = () => {
+    setQuestionnaire(questionsBank);
+    setScore(0);
+    setResponses(0);
+    setShowResult(false)
+    setIsPlayingAgain(true)
+  };
+
+  const sendScore = async () => {
+    try {
+      const newGame = {questionary_id: questionnaire._id, score}
+      // if(isPlayingAgain){
+      //   const response = await Game.update(newGame)
+      // }
+      console.log({newGame})
+      const response = await Game.create(newGame)
+      if(response.data){
+        alert('Cuestionario enviado')
+      }
+      setShowResult(true);
+    } catch (e) {
+      console.log("something went wrong", e);
+    }
+  };
+
+  return (
+    <section className={styles.questionnaireSection}>
+      <header className={styles.header}>{questionnaire.name}</header>
+      {!showResult && (
+        <ListOfQuestions
+          questions={questionnaire.questions}
+          computeAnswer={computeAnswer}
+        />
+      )}
+      {responses === questionnaire.questions.length && !showResult && (
+        <Button variant="success" onClick={sendScore}>
+          Finalizar
+        </Button>
+      )}
+      {showResult && (
+        <Result
+          score={score}
+          length={questionnaire.questions.length}
+          playAgain={playAgain}
+        />
+      )}
+    </section>
+  );
 };
 
-export default Game;
+export default GamePage;
+
+export async function getServerSideProps(context) {
+  const { id, token } = context.query;
+  api.defaults.headers.common["x-access-token"] = token;
+  try {
+    const response = await api.get(`/questionaries/${id}`);
+    const questionsBank = await response.data;
+    return {
+      props: { questionsBank },
+    };
+  } catch (e) {
+    console.log("Something went wrong", e);
+  }
+}
