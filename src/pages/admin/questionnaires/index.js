7a2853b8c6b49fb1bbb023751d7a6e1b3a177b5e
Nathaly Bermeo <nathaly.bermeo@epn.edu.ec>
2022-01-21 13:43:52 -0500

    implements-questionary

--- src/pages/admin/questionnaires/index.js
+++ src/pages/admin/questionnaires/index.js
@@ -1,83 +1,29 @@
 import { useRouter } from "next/router";
-import { Button, Card, Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { ListOfQuestionnaires } from "../../../components/ListOfQuestionnaires";
 import { ROUTES } from "../../../constants/routes";
 import { withPrivate } from "../../../hocs/withPrivate";
-import styles from "./style.module.css";
-import { useState } from "react";
+import { useQuestionnaire } from "../../../hooks/useQuestionnaire";
+import styles from './style.module.css'
 
-const QUESTIONS = {
-    questionName: "question1",
-    optionName: "question1Option1",
-};
-
-const CreateQuestionnaire = () => {
-    const router = useRouter();
-    const handleSubmit = () => {};
-    const onSubmit = () => {};
-    const [questionsGroup, setQuestionsGroup] = useState([QUESTIONS]);
-    console.log({questionsGroup})
-
-    const addQuestion = () => {
-        const length = questionsGroup.length;
-        const newQuestion = {
-            questionName: `question${length + 1}`,
-            optionName: `question${length + 1}Option1`,
-        };
-        setQuestionsGroup((prevState) => [...prevState, newQuestion]);
-    };
-
+const Questionnaires = () => {
+    const { questionnaires } = useQuestionnaire();
+    const router = useRouter()
     return (
         <>
             <div className={styles.header}>
-                <h2>Creación de cuestionarios</h2>
-                <Button variant="primary" size="sm"onClick={() => router.push(ROUTES.CREATE_QUESTIONNAIRE)}>
+                <h2>Cuestionarios</h2>
+                <Button
+                    variant="outline-primary"
+                    size="sm"
+                    onClick={() => router.push(ROUTES.CREATE_QUESTIONNAIRE)}
+                >
                     Crear
                 </Button>
             </div>
-
-            <Form
-                id="create-questionnaire-form"
-                onSubmit={handleSubmit(onSubmit)}
-                className={styles.form}
-            >
-                <Form.Group className={styles.groupForm}>
-                    <Form.Control
-                        type="text"
-                        placeholder="Título del cuestionario"
-                        //{...register("title")}
-                        //isInvalid={!!errors.title?.message}
-                    />
-                    {/* <Form.Control.Feedback type="invalid">
-              {errors.title?.message}
-            </Form.Control.Feedback> */}
-                </Form.Group>
-                {questionsGroup.map((question, id) => {
-                    return (
-                        <Form.Group className={styles.groupForm} key={id}>
-                            <Form.Control
-                                type="text"
-                                placeholder="Pregunta"
-                                name={question.questionName}
-                            />
-                            <Form.Control
-                                style={{marginTop: '8px'}}
-                                type="text"
-                                placeholder="Opción 1"
-                                name={question.optionName}
-                            />
-                            {id === questionsGroup.length - 1 ? (
-                                <Button size="sm" style={{ marginTop: "10px" }} onClick={addQuestion}>
-                                    Añadir pregunta
-                                </Button>
-                            ) : (
-                                ""
-                            )}
-                        </Form.Group>
-                    );
-                })}
-            </Form>
+            <ListOfQuestionnaires questionnaires={questionnaires} />
         </>
     );
 };
 
-export default withPrivate(CreateQuestionnaire);
+export default withPrivate(Questionnaires);
