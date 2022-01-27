9c86465c995c2ccab20daa39a1d5989548f838c6
NathalyBermeo97 <nathaly.bermeo@epn.edu.ec>
2022-01-27 12:13:32 -0500

    test event modal fail yet

--- src/components/EventModal/EventModal.test.js
+++ src/components/EventModal/EventModal.test.js
@@ -1,6 +1,15 @@
-import { prettyDOM, render } from "@testing-library/react";
+import {
+  createEvent,
+  fireEvent,
+  prettyDOM,
+  render,
+} from "@testing-library/react";
 import { EventModal } from ".";
 import { mockNextUseRouter } from "../../test_util";
+
+// jest.spyOn(require("../../context/AuthContext"), 'useAuth').mockImplementation(() => ({
+//   currentUser: { _id: "1212", name: "migue" },
+// }));
 
 describe("Event modal testing", () => {
   mockNextUseRouter({
@@ -25,12 +34,12 @@ describe("Event modal testing", () => {
       title: "new event",
       description: "description",
       number: 5,
-      inscriptions: [1, 2, 3, 4, 5],
+      inscriptions: ["1212", 2, 3, 4, 5],
     };
 
     const component = render(<EventModal isOpen={true} event={event} />);
-    const button = component.queryByText("Sin cupones");
-    console.log(prettyDOM(button));
-    expect(button).toHaveTextContent("Sin cupones");
+    const button = component.getByText("Sin cupones");
+    expect(button.getAttribute('disabled')).toBe(true)
+    // expect(button).toHaveTextContent("Sin cupones");
   });
 });
