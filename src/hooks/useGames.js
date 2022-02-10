ff3b055d683dc9bf15e800ba3f50b391e4e3c987
NathalyBermeo97 <nathaly.bermeo@epn.edu.ec>
2022-02-10 09:52:29 -0500

    implement re-attempt game

--- src/hooks/useGames.js
+++ src/hooks/useGames.js
@@ -1,47 +1,81 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import Games from "../api/games";
+import Games from "../api/score";
+import { useAuth } from "../context/AuthContext";
 
 export const useGames = () => {
-    const [games, setGames] = useState([]);
+  const [games, setGames] = useState([]);
+  const {currentUser} = useAuth()
+  console.log({games})
+  useEffect(() => {
+    const getGames = () => {
+      Games.get()
+        .then((response) => {
+          console.log({ response });
+          setGames(response.data);
+        })
+        .catch((err) => console.log("something went wrong", err));
+    };
+    getGames();
+  }, []);
 
-    useEffect(() => {
-        const getGames= () => {
-            Games.get()
-                .then(response => {
-                    console.log({response})
-                    setGames(response.data)
-                }).catch(err => console.log('something went wrong', err));
-        };
-        getGames();
-    }, []);
-
-    const updateGames = async (id, newNewsItem) => {
-        try{
-            const response = await Games.update(id, newNewsItem)
-            return response.data.message
-        }catch(e){
-            console.log('Something went wrong', e)
-        }
+  const updateGames = async (id, newNewsItem) => {
+    try {
+      const response = await Games.update(id, newNewsItem);
+      return response.data.message;
+    } catch (e) {
+      console.log("Something went wrong", e);
     }
+  };
 
-    const createGamesItem = async (newGamesItem) => {
-        try{
-            const response = await Games.create(newGamesItem)
-            return response.data.message
-        }catch(e){
-            console.log('something went wrong', e)
-        }
+  const createGamesItem = async (newGamesItem) => {
+    try {
+      const response = await Games.create(newGamesItem);
+      return response.data.message;
+    } catch (e) {
+      console.log("something went wrong", e);
     }
-    const deleteGames = async (newGamesItem) => {
-        try{
-            const response = await Games.delete(newGamesItem)
-            return response.data.message
-        }catch(e){
-            console.log('something went wrong', e)
-        }
+  };
+  const deleteGames = async (newGamesItem) => {
+    try {
+      const response = await Games.delete(newGamesItem);
+      return response.data.message;
+    } catch (e) {
+      console.log("something went wrong", e);
     }
+  };
 
+  const getUserQuestionnaireScore = async (questionnaireId, userId) => {
+    try {
+      const response = await Games.get();
+      return response.data.filter(
+        (game) =>
+          game.user_id === userId && game.questionary_id === questionnaireId
+      );
+    } catch (e) {
+      console.log("something went wrong", e);
+    }
+  };
 
-    return {games, setGames,deleteGames,updateGames, createGamesItem};
+  const hasAlreadyAttemptedGame = (questionnaireId) => {
+    // try {
+    //   const response = await Games.get();
+      return games.filter(
+        (game) =>
+          game.user_id === currentUser._id
+      ).some(game => game.questionary_id === questionnaireId)
+    // } catch (e) {
+    //   console.log("something went wrong", e);
+    // }
+  };
+
+  return {
+    games,
+    setGames,
+    deleteGames,
+    updateGames,
+    createGamesItem,
+    getUserQuestionnaireScore,
+    hasAlreadyAttemptedGame
+  };
 };
